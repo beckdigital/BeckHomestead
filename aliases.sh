03f0eb2254e57716963593675b7f2172ff2d23d7
Nathan Morgan <nathan@fuelingtheweb.com>
2016-11-10 14:12:04 -0500

    Add alias for sshing directing into a project folder

--- aliases.sh
+++ aliases.sh
@@ -7,4 +7,10 @@ function beck-run() {
     echo $(beck ssh -- -t "cd projects/$PROJECTPATH && $*")
 }
 
+function beck-ssh() {
+    PROJECTPATH=${$(pwd)##*Projects/Beck/development/}
+    ssh -p 2222 vagrant@localhost -t "cd projects/$PROJECTPATH ; /bin/bash"
+}
+
 alias br='beck-run'
+alias bssh='beck-ssh'
